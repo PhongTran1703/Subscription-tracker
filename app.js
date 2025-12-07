8dc77798c81974ae79f00b6703a6c3aac462eec6
Phong Tran <thanhphongtran1703@gmail.com>
2025-12-07 13:58:15 +0000

    finished on config part

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Server is running on port 3000')
+import express from 'express';
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+});
+
+export default app;
