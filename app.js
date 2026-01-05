5b84edc43316a79f002ab92cdcde19af22085b49
Phong Tran <thanhphongtran1703@gmail.com>
2026-01-05 19:59:06 +0000

    tesing auth

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //api/v1/auth/sign-up
