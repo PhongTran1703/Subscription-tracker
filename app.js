2a3d38b5e17e512cf628dad97102016517a3349b
Phong Tran <thanhphongtran1703@gmail.com>
2026-01-06 18:26:46 +0000

    feat(arjet): Add limit for request on Arcjet

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/middle.error.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(arcjetMiddleware);
 //api/v1/auth/sign-up
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
---
+++ config/arcjet.js
@@ -0,0 +1,33 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import { isSpoofedBot } from "@arcjet/inspect";
+import express from "express";
+import { ARCJET_KEY } from "./env.js";
+
+
+
+const aj = arcjet({
+  key: ARCJET_KEY,
+  rules: [
+   
+    shield({ mode: "LIVE" }),
+    
+    detectBot({
+      mode: "LIVE", 
+      
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+      ],
+    }),
+     tokenBucket({
+      mode: "LIVE",
+      // Tracked by IP address by default, but this can be customized
+      // See https://docs.arcjet.com/fingerprints
+      //characteristics: ["ip.src"],
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+  ],
+});
+
+export default aj;
